881261fa6a5032eeeee38540e67e99d046c6cddb
wubu <wubeabera123@gmail.com>
2026-02-16 22:30:57 +0300

    successfully connected fastapi postgres db in prisma

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from app.routers import tasks
-
+from app.services.prisma import db
 
 app = FastAPI()
-app = FastAPI(title="TaskFlow API")
 
 app.include_router(tasks.router)
 
-@app.get("/")
-def home():
-    return {"message": "TaskFlow API is running"}
+
+@app.on_event("startup")
+async def startup():
+    await db.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.disconnect()
--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,40 +1,45 @@
 from fastapi import APIRouter, HTTPException
-from app.schemas.task import Task, TaskCreate
+from app.schemas.task import TaskCreate
+from app.services.prisma import db
 
 router = APIRouter()
 
-# Fake in-memory "database"
-tasks = []
-task_id_counter = 1
 
-# Create Task
-@router.post("/tasks", response_model=Task)
-def create_task(task: TaskCreate):
-    global task_id_counter
-    new_task = Task(id=task_id_counter, **task.dict())
-    tasks.append(new_task)
-    task_id_counter += 1
+# CREATE TASK
+@router.post("/tasks")
+async def create_task(task: TaskCreate):
+    new_task = await db.task.create(
+        data={
+            "title": task.title,
+            "description": task.description,
+            "userId": 1   # temporary until auth phase
+        }
+    )
     return new_task
 
-# Get All Tasks
-@router.get("/tasks", response_model=list[Task])
-def get_tasks():
-    return tasks
 
-# Get Task by ID
-@router.get("/tasks/{task_id}", response_model=Task)
-def get_task(task_id: int):
-    for task in tasks:
-        if task.id == task_id:
-            return task
-    raise HTTPException(status_code=404, detail="Task not found")
+# GET ALL
+@router.get("/tasks")
+async def get_tasks():
+    return await db.task.find_many()
 
-# Delete Task by ID
-@router.delete("/tasks/{task_id}", response_model=dict)
-def delete_task(task_id: int):
-    global tasks
-    for task in tasks:
-        if task.id == task_id:
-            tasks = [t for t in tasks if t.id != task_id]
-            return {"message": "Task deleted"}
-    raise HTTPException(status_code=404, detail="Task not found")
+
+# GET ONE
+@router.get("/tasks/{task_id}")
+async def get_task(task_id: int):
+    task = await db.task.find_unique(where={"id": task_id})
+
+    if not task:
+        raise HTTPException(404, "Task not found")
+
+    return task
+
+
+# DELETE
+@router.delete("/tasks/{task_id}")
+async def delete_task(task_id: int):
+    try:
+        await db.task.delete(where={"id": task_id})
+        return {"message": "Task deleted"}
+    except:
+        raise HTTPException(404, "Task not found")
